9cb2b304b09a800c785fb071e971e227f3f22dbc
Ricardo Ebbers <recl@cin.ufpe.br>
2020-12-09 21:56:54 -0300

    Refactored build.sh

--- synchronous-example/build.sh
+++ synchronous-example/build.sh
@@ -3,10 +3,10 @@ projectName="synchronous-example"
 echo "building services of $projectName..."
 for d in */; do
     echo "building '$d' service..."
-    cd $d
+    cd "$d" || exit
     imageName="$projectName/${d%/}"
     echo "creating '$imageName' image..."
-    ./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=$imageName
+    ./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName="$imageName"
     cd ..
 done
 echo "building done."
